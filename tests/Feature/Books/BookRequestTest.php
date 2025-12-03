0b80a9ed7bc79d2800c39bd8f5cb1c5a69d9386b
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-12-03 23:35:56 +0000

    Add BookRequestTest

---
+++ tests/Feature/Books/BookRequestTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use App\Models\Book;
+use App\Models\Publisher;
+use App\Models\Submission;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Role;
+
+uses(RefreshDatabase::class);
+
+test('user can create a book request', function () {
+    /** @var \Tests\TestCase $this */
+
+    Role::create(['name' => 'citizen', 'guard_name' => 'web']);
+    Role::create(['name' => 'admin', 'guard_name' => 'web']);
+
+    $user = User::factory()->create();
+     /** @var \App\Models\User $user */
+    $user->assignRole('citizen');
+
+    $publisher = Publisher::factory()->create();
+    $book = Book::factory()->create(['publisher_id' => $publisher->id]);
+
+    $response = $this->actingAs($user)
+        ->post(route('submissions.store'), [
+            'book_id' => $book->id
+        ]);
+
+    $response->assertRedirect(route('submissions.index'));
+
+    $this->assertDatabaseHas('submissions', data: [
+        'user_id' => $user->id,
+        'book_id' => $book->id,
+        'status' => 'created'
+    ]);
+
+    $submission = Submission::where('user_id', $user->id)->latest()->first();
+    expect($submission->expected_return_date->format('Y-m-d'))
+        ->toBe(now()->addDays(5)->format('Y-m-d'));
+});
